2949dd9647e18fb14e08c16db7f8a2ea98f60e2c
Matthew Avant <matthew.avant@gmail.com>
2014-03-18 15:08:43 -0400

    Added rudimentary support for passing arguments on the command line.

--- rustoleum.rs
+++ rustoleum.rs
@@ -1,30 +1,61 @@
-use std::io::{stdin, File, timer};
+use std::io::{stdin, File};
 use std::io::buffered::BufferedReader;
 use std::str::{from_utf8};
-use std::vec::ChunkIter;
-use std::vec::OwnedStr;
+use std::os;
+
+fn print_usage () {
+	println("A tool to keep your reading skills bright and shiny.");
+	println("Usage: rustoleum [arguments] path/to/input/file");
+	println("-i (I)nteractive mode");
+	println("-wpm N Display N words per (m)inute (default: 300)");
+	println("-wpl N Display N words per (l)ine (default: 1)");
+	println("-h Display this (h)elp");
+}
+
+fn get_args_interactive () -> (uint, uint, Path) {
+	let mut reader = BufferedReader::new(stdin());
+
+	println("Input path to file to read: ");
+	let input = reader.read_line().unwrap_or(~"data/example.txt");
+	let p = Path::new(input.trim());
+	
+	println("Input WPM: ");
+	let input = reader.read_line().unwrap();
+	let WPM = from_str::<uint>(input.trim()).unwrap_or(300);
+
+	println("Input the number of words to view at a time: ");
+	let input = reader.read_line().unwrap();
+	let WPL = from_str::<uint>(input.trim()).unwrap_or(1);
+
+	(WPM, WPL, p)
+}
+
+fn get_args () -> (uint, uint, Path){
+	let args = os::args();
+	if args.contains(&~"-h"){
+		print_usage();
+	} 
+	if args.contains(&~"-i"){
+		get_args_interactive()
+	}  else {
+		let fallbackfile = &~"data/example.txt";
+		let path = args.tail().last_opt().unwrap_or(fallbackfile);
+		let p = Path::new(path.as_slice());
+		let WPM = from_str::<uint>(args[1 + args.position_elem(&~"-wpm").unwrap_or(-1)]).unwrap_or(300);
+		let WPL = from_str::<uint>(args[1 + args.position_elem(&~"-wpl").unwrap_or(-1)]).unwrap_or(1);
+		(WPM,WPL,p)
+	}
+}
 
 fn main () {
-  let p = Path::new("data/example.txt");
-  let contents = File::open(&p).read_to_end();
-  let string_contents = from_utf8(contents).replace("\n", " ");
-  let mut string_iter = string_contents.split_str(" ");
-  let mut reader = BufferedReader::new(stdin());
-  println("Input WPM: ");
-  let input = reader.read_line().unwrap();
-  let WPM = from_str::<uint>(input.trim()).unwrap_or(300);
-
-  println("Input the number of words to view at a time: ");
-  let input = reader.read_line().unwrap();
-  let words_at_a_time = from_str::<uint>(input.trim()).unwrap_or(1);
-
-  let sleep_time = 60000 / WPM * words_at_a_time;
-
-  let string_vec = string_iter.to_owned_vec();
-
-  for words in string_vec.chunks(words_at_a_time) {
-    println("\x1bc".into_owned().append(words.connect(" ")));
-    std::io::timer::sleep(sleep_time.to_u64().unwrap());
-  }
+	let (WPM, WPL, p) = get_args();
+	let contents = File::open(&p).read_to_end();
+	let mut string_iter = from_utf8(contents).words();
+	let string_vec = string_iter.to_owned_vec();
+	let sleep_time = 60000 / WPM * WPL;
+	for words in string_vec.chunks(WPL) {
+		println("\x1bc".into_owned().append(words.connect(" ")));
+		std::io::timer::sleep(sleep_time.to_u64().unwrap());
+	}
 
 }
