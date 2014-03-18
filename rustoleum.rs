4da4f0f41252dc7ac1f6dba904055bced19f6760
Matthew Avant <matthew.avant@gmail.com>
2014-03-18 15:36:53 -0400

    Improved support for command-line arguments.

--- rustoleum.rs
+++ rustoleum.rs
@@ -18,7 +18,7 @@ fn get_args_interactive () -> (uint, uint, Path) {
 	println("Input path to file to read: ");
 	let input = reader.read_line().unwrap_or(~"data/example.txt");
 	let p = Path::new(input.trim());
-	
+
 	println("Input WPM: ");
 	let input = reader.read_line().unwrap();
 	let WPM = from_str::<uint>(input.trim()).unwrap_or(300);
@@ -30,11 +30,7 @@ fn get_args_interactive () -> (uint, uint, Path) {
 	(WPM, WPL, p)
 }
 
-fn get_args () -> (uint, uint, Path){
-	let args = os::args();
-	if args.contains(&~"-h"){
-		print_usage();
-	} 
+fn get_vals (args: ~[~str]) -> (uint, uint, Path){
 	if args.contains(&~"-i"){
 		get_args_interactive()
 	}  else {
@@ -47,15 +43,26 @@ fn get_args () -> (uint, uint, Path){
 	}
 }
 
+fn print_file(WPM: uint, WPL: uint, p: Path){
+	if p.exists(){
+		let contents = File::open(&p).read_to_end();
+		let mut string_iter = from_utf8(contents).words();
+		let string_vec = string_iter.to_owned_vec();
+		let sleep_time = 60000 / WPM * WPL;
+		for words in string_vec.chunks(WPL) {
+			println("\x1bc".into_owned().append(words.connect(" ")));
+			std::io::timer::sleep(sleep_time.to_u64().unwrap());
+		}
+	} else {
+		println("The path specified does not exist. Please specify a valid path, or run rustoleum -h for help.");
+	}
+}
 fn main () {
-	let (WPM, WPL, p) = get_args();
-	let contents = File::open(&p).read_to_end();
-	let mut string_iter = from_utf8(contents).words();
-	let string_vec = string_iter.to_owned_vec();
-	let sleep_time = 60000 / WPM * WPL;
-	for words in string_vec.chunks(WPL) {
-		println("\x1bc".into_owned().append(words.connect(" ")));
-		std::io::timer::sleep(sleep_time.to_u64().unwrap());
+	if os::args().contains(&~"-h"){
+		print_usage();
+	} else {
+		let (WPM, WPL, p) = get_vals(os::args());
+		print_file(WPM, WPL, p);
 	}
 
 }
